6c2e073d8115868ab2abd8d6798ef1dc83ea35a4
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-09 02:06:22 +0530

    Auto Push

--- expense/backend.sh
+++ expense/backend.sh
@@ -5,11 +5,12 @@ dnf install nodejs -y
 useradd expense
 mkdir /app 
 curl -o /tmp/backend.zip https://expense-web-app.s3.amazonaws.com/backend.zip 
+
+cp backend.service  /etc/systemd/system/backend.service
 cd /app 
 unzip /tmp/backend.zip
 cd /app 
 npm install 
-cp backend.service  /etc/systemd/system/backend.service 
 chmod -R 775 /app
 chown -R expense:expense /app
 dnf install mysql-server -y
